473727844a0c6d73591a00f538ace0194c97bd1d
guptajeetesh7 <gupta.jeetesh7@gmail.com>
2017-07-24 22:54:34 +0530

    Register User to Firebase

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,61 @@
-var app = angular.module('firebook',[]);
+var app = angular.module('firebook',['ngRoute','firebase']);
+
+//Angular Routing
+app.config(function($routeProvider,$locationProvider){
+
+	$routeProvider.when('/',{
+		templateUrl : 'views/Login.html',
+		controller: 'register'
+	});
+
+
+	  // enable html5Mode for pushstate ('#'-less URLs)
+
+    
+
+});
 
 
 
+app.controller('register',function($scope,$location,$firebaseAuth){
+
+	var ref = firebase.auth();  
+    var authObj = $firebaseAuth(ref);
+
+    console.log(authObj);
+
+	$scope.reg = function(){
+		if($scope.name!=null && $scope.email!=null && $scope.password!=null && $scope.password_again!=null){
+
+			if($scope.password == $scope.password_again){
+					
+				//create users
+				authObj.$createUserWithEmailAndPassword($scope.email, $scope.password).then(function(){
+
+					alert('Thank You for Registeration');
+
+				}).catch(function(error) {
+				  // Handle Errors here.
+				  
+				  alert(error.message);
+				  // ...
+				});
+
+
+			}else{
+				alert('Password not Matched!!');
+			}
+
+		}else{
+			alert('Please Fill up all feilds');
+		}
+		
+	};
+
+	$scope.signin = function(){
+		
+		
+	};
+
+});
 
